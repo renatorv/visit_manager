25aa7e9fb14225dec4c75475f2c9e7dce468a96b
renatoalves <renatorv@gmaail.com>
2026-04-19 21:54:27 -0300

    Adicionado delay de 2 segundos na splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'core/di/injection_container.dart';
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await setupDependencies();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await Future.wait([
+    setupDependencies(),
+    Future.delayed(const Duration(seconds: 2)),
+  ]);
+
+  FlutterNativeSplash.remove();
   runApp(const VisitManagerApp());
 }
 
@@ -34,3 +42,4 @@ class VisitManagerApp extends StatelessWidget {
     );
   }
 }
+
